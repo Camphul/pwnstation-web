d154b612a42cd6e409c3c46fa1e8a5b54c78cd6f
L.M. Camphuisen <luca.camphuisen@hva.nl>
2021-07-24 08:22:57 +0200

    Update pwn station url

--- plugins/socket-client-plugin.js
+++ plugins/socket-client-plugin.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client'
 import { handleClientConnectionStatus, isClientConnected } from "~/assets/pwnsocket/client";
 export default ({ app, store, env, $config }, inject) => {
-  const socket = io($config.WS_URL)
+  const socket = io("ws://pwn.station:3000")
   inject('socket', socket)
   inject('isConnected', () => isClientConnected(store))
   handleClientConnectionStatus(store, socket)
