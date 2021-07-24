f01e0e85f9591e437da7b150c14effdbc6d11c9f
L.M. Camphuisen <luca.camphuisen@hva.nl>
2021-07-24 16:04:39 +0200

    Disable dependency on cdns.

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@mdi/font/css/materialdesignicons.min.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -43,6 +44,7 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
+    ['@nuxtjs/vuetify', { iconfont: 'mdi' }]
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -71,6 +73,7 @@ export default {
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
+    defaultAssets: false,
     theme: {
       dark: true,
       themes: {
